87d888673012d5c70dde7ad3417c24d9abf2e50f
Quentin-Jadoul <18155@ecam.be>
2022-11-18 15:30:58 +0100

    Created order service

---
+++ sylla/src/main/java/com/RISE/sylla/service/orderService.java
@@ -0,0 +1,36 @@
+package com.RISE.sylla.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class orderService {
+    @Autowired
+    com.RISE.sylla.repository.orderRepository orderRepository;
+
+    // CREATE
+    public orderModel createOrder(orderModel order) {
+        return orderRepository.save(order);
+    }
+
+    // READ
+    public List<orderModel> getOrder() {
+        return orderRepository.findAll();
+    }
+
+    // DELETE
+    public void deleteOrder(Long orderId) {
+        orderRepository.deleteById(orderId);
+    }
+
+    // UPDATE
+    public orderModel updateOrder(Long orderId, orderModel orderDetails) {
+        orderModel order = orderRepository.findById(orderId).get();
+        order.setStudent(orderDetails.getStudent());
+        order.setDate(orderDetails.getDate());
+        order.setPrice(orderDetails.getPrice());
+        order.setState(orderDetails.getState());
+
+        return orderRepository.save(order);
+    }
+}
