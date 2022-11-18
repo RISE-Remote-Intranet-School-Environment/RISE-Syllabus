2bbde4fd150cb13451d23974d32ec7e6891fd705
olingo99 <engels.theo@skynet.be>
2022-11-18 14:01:05 +0100

    adding project

---
+++ sylla/src/main/java/com/RISE/sylla/SyllaApplication.java
@@ -0,0 +1,13 @@
+package com.RISE.sylla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyllaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyllaApplication.class, args);
+	}
+
+}
---
+++ sylla/src/test/java/com/RISE/sylla/SyllaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RISE.sylla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyllaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
