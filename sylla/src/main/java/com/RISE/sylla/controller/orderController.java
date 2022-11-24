d6db4c50a339a5b2fd903703524c5d3ef1701c93
KuijpersNick0 <kuijpers2nick@gmail.com>
2022-11-24 16:15:36 +0100

    orderController

---
+++ sylla/src/main/java/com/RISE/sylla/controller/orderController.java
@@ -0,0 +1,47 @@
+package com.RISE.sylla.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.RISE.sylla.model.ordersModel;
+import com.RISE.sylla.service.orderService;
+
+@RestController
+@RequestMapping("/api")
+public class orderController {
+    @Autowired
+    orderService orderService;
+
+    @RequestMapping(value="/orders", method= RequestMethod.POST)
+    public ordersModel createOrder(@RequestBody ordersModel order) {
+        return orderService.createOrder(order);
+    }
+
+    @RequestMapping(value="/orders", method=RequestMethod.GET)
+    public List<ordersModel> readOrders() {
+        return orderService.getOrder();
+    }
+
+    @RequestMapping(value="/orders/{orderId}", method=RequestMethod.PUT)
+    public orderModel readOrders(@PathVariable(value = "orderId") Long id, @RequestBody ordersModel orderDetails) {
+        return orderService.updateOrder(id, orderDetails);
+    }
+
+    @RequestMapping(value="/orders/{orderId}", method=RequestMethod.DELETE)
+    public void deleteOrder(@PathVariable(value = "orderId") Long id) {
+        orderService.deleteOrder(id);
+    }
+
+}
--- sylla/src/main/java/com/RISE/sylla/controller/userController.java
+++ sylla/src/main/java/com/RISE/sylla/controller/userController.java
@@ -1,4 +1,4 @@
-package com.RISE.sylla.controller;
+git stapackage com.RISE.sylla.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
