059ddf7e7f8925fba2888d9aa1929e4d21b9d5a3
olingo99 <engels.theo@skynet.be>
2022-11-25 13:37:54 +0100

    added mapdocuorder controller

--- sylla/src/main/java/com/RISE/sylla/controller/courseController.java
+++ sylla/src/main/java/com/RISE/sylla/controller/courseController.java
@@ -1,4 +1,4 @@
-git stapackage com.RISE.sylla.controller;
+package com.RISE.sylla.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
