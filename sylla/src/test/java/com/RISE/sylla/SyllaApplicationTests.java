0f8ae73f1835c0e2e6a3f8db7eca99a8b5ddbe15
Bdone-hub <67092457+Bdone-hub@users.noreply.github.com>
2022-12-02 16:03:17 +0100

    add unit_test

--- sylla/src/test/java/com/RISE/sylla/SyllaApplicationTests.java
+++ sylla/src/test/java/com/RISE/sylla/SyllaApplicationTests.java
@@ -1,7 +1,10 @@
 package com.RISE.sylla;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 
 @SpringBootTest
 class SyllaApplicationTests {
