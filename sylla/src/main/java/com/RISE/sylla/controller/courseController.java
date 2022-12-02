c95fe73da5eb5e1de41cf4ef973557f317034127
KuijpersNick0 <kuijpers2nick@gmail.com>
2022-12-02 14:21:19 +0100

    i

--- sylla/src/main/java/com/RISE/sylla/controller/courseController.java
+++ sylla/src/main/java/com/RISE/sylla/controller/courseController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
+import java.util.List;
+
 import java.util.Optional;
 
 import org.springframework.web.bind.annotation.PathVariable;
