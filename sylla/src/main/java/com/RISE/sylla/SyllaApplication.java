8ee8c8da2b97adc55af8542590b06c97c00b4adf
corentin <21306@ecam.be>
2023-11-17 14:37:11 +0100

    Testing

--- sylla/src/main/java/com/RISE/sylla/SyllaApplication.java
+++ sylla/src/main/java/com/RISE/sylla/SyllaApplication.java
@@ -1,13 +1,17 @@
 package com.RISE.sylla;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SyllaApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(SyllaApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SyllaApplication.class, args);
 	}
-
 }
