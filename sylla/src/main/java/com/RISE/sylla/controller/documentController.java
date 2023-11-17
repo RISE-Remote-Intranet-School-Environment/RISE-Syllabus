ea20149d4853650b1d61918c9f221bdba9e6e44f
radmou99 <mouradm.mm@outlook.com>
2023-11-17 14:50:49 +0100

    try

--- sylla/src/main/java/com/RISE/sylla/controller/documentController.java
+++ sylla/src/main/java/com/RISE/sylla/controller/documentController.java
@@ -56,6 +56,13 @@ public class documentController {
         return documentService.getDocuments();
     }
 
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, documentModel model) {
+
+        return "greeting";
+    }
+
+
     /**
      * return a specific document by its id
      *
