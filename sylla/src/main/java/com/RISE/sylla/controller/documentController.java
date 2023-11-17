5030d49d0ed53872321f515484bb472d2749d00b
radmou99 <mouradm.mm@outlook.com>
2023-11-17 15:13:40 +0100

    corrected document controller
    Signed-off-by: radmou99 <mouradm.mm@outlook.com>

--- sylla/src/main/java/com/RISE/sylla/controller/documentController.java
+++ sylla/src/main/java/com/RISE/sylla/controller/documentController.java
@@ -58,11 +58,6 @@ public class documentController {
         return documentService.getDocuments();
     }
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, documentModel model) {
-
-        return "greeting";
-    }
 
 
     /**
