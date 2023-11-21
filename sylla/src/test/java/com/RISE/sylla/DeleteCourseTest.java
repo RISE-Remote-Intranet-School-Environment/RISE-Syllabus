56e561d77ef1e231a838c8541f0b82b3365896b2
corentin <21306@ecam.be>
2023-11-21 18:45:11 +0100

    Unit Test

--- sylla/src/test/java/com/RISE/sylla/DeleteCourseTest.java
+++ sylla/src/test/java/com/RISE/sylla/DeleteCourseTest.java
@@ -1,45 +1,61 @@
 package com.RISE.sylla;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class DeleteCourseTest extends SyllaApplicationTests {
     @Value("${server.port}")
     private int port;
     @Autowired
+    private WebApplicationContext webApplicationContext;
+
     private MockMvc mvc;
 
-    DeleteCourseTest() {
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
+
 
     @Test
     public void deleteCourseTest() throws Exception {
-        String json = "{\"courseId\":10},{\"name\":\"nameTest\"},{\"teacher\":\"teacherTest\",{\"ue\":\"ueTest\",{\"year\":707}";
+        String json = "{\"courseId\":10,\"name\":\"nameTest\",\"teacher\":\"teacherTest\",\"ue\":\"ueTest\",\"year\":707}";
 
-        mvc.perform(post("http://localhost:" + port + "/courses/")
+        mvc.perform(post("http://localhost:" + port + "/courses")
                         .content(json)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-/*
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.courseId").value(10))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("nameTest"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.teacher").value("teacherTest"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.ue").value("ueTest"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.year").value(707));
+
         mvc.perform(delete("http://localhost:" + port + "/courses/10")
                         .content(json)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
 
- */
     }
 }
---
+++ sylla/src/test/java/com/RISE/sylla/GetCourseUnitTest.java
@@ -0,0 +1,90 @@
+import com.RISE.sylla.model.courseModel;
+import com.RISE.sylla.service.courseService;
+import com.RISE.sylla.controller.courseController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class GetCourseUnitTest {
+
+    @Mock
+    private courseService courseService;
+
+    @InjectMocks
+    private courseController courseController;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(courseController).build();
+    }
+
+    @Test
+    public void testGetCourseById() throws Exception {
+        // Mocking the service layer to return a specific course by ID
+        courseModel course = new courseModel();
+        course.setCourseId(1L);
+        course.setName("Course1");
+        course.setTeacher("Teacher1");
+        course.setUE("UE1");
+        course.setYear(2022);
+        when(courseService.getCourseById(1L)).thenReturn(Optional.of(course));
+
+        // Perform the GET request and validate the response
+        mockMvc.perform(get("/courses/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.courseId").value(1))
+                .andExpect(jsonPath("$.name").value("Course1"));
+    }
+
+    @Test
+    public void testGetAllCourses() throws Exception {
+        courseModel course1 = new courseModel();
+        course1.setCourseId(1L);
+        course1.setName("Course1");
+        course1.setTeacher("Teacher1");
+        course1.setUE("UE1");
+        course1.setYear(2022);
+
+        courseModel course2 = new courseModel();
+        course2.setCourseId(2L);
+        course2.setName("Course2");
+        course2.setTeacher("Teacher2");
+        course2.setUE("UE2");
+        course2.setYear(2022);
+        // Mocking the service layer to return a list of courses
+        List<courseModel> courses = Arrays.asList(
+                course1,
+                course2
+        );
+
+        when(courseService.getCourses()).thenReturn(courses);
+
+        // Perform the GET request and validate the response
+        mockMvc.perform(get("/courses")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].courseId").value(1))
+                .andExpect(jsonPath("$[0].name").value("Course1"))
+                .andExpect(jsonPath("$[1].courseId").value(2))
+                .andExpect(jsonPath("$[1].name").value("Course2"));
+    }
+}
